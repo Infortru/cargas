be76e87dd8276021dbac5b20a959c37d6d4b059f
Infortru <ramontru1@hotmail.com>
2025-05-04 20:24:26 +0200

    volver

--- validar_rec_carga.js
+++ validar_rec_carga.js
@@ -4,21 +4,18 @@ let formulario = document.getElementById('formulario');
 let inputs = document.querySelectorAll('#formulario input');
 
 let expresiones={
-    fecha: /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{2}$/,
+    //fecha: /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{2}$/,
     tecnico: /^[a-zA-ZÀ-ÿ\s]{4,15}$/
 }
 
 let entradas={
-    fecha: true,
     tecnico: false
 }
 
 function validarFormulario(e){
 
     switch (e.target.name){
-        case "fecha":
-            validarEntrada(expresiones.fecha, e.target, "fecha");
-        break;
+        
         case "tecnico":
             validarEntrada(expresiones.tecnico, e.target, "tecnico");
         break;
@@ -33,14 +30,10 @@ function validarEntrada(expresion, input, entrada){
         entradas[entrada] = false
     }
     
-    if((entradas.fecha && entradas.tecnico)==true){
+    if((entradas.tecnico)==true){
    
         btnEnviar = document.querySelector("#enviar");
         btnEnviar.disabled=false;
     }
 }
-inputs.forEach((input) => {
-	input.addEventListener('keyup', validarFormulario);
-	input.addEventListener('blur', validarFormulario);
-});
 
